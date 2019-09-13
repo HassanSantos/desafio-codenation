841f48fed0fec16ce1eccb9edd7a4114f488a2d2
HassanSena <hassanssena@gmail.com>
2019-09-13 00:26:39 -0300

    Desafio completo. Falta apenas teste unitário

--- src/main/java/br/com/codenation/RestClient.java
+++ src/main/java/br/com/codenation/RestClient.java
@@ -1,31 +1,44 @@
 package br.com.codenation;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.sun.jersey.multipart.MultiPart;
+import com.sun.jersey.multipart.file.StreamDataBodyPart;
+import com.sun.jersey.multipart.impl.MultiPartWriter;
+
 public class RestClient {
-	private final String URI = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution";
-	private final String PATH = "challenge/dev-ps/submit-solution";
+	private final String URI = "https://api.codenation.dev/v1";
+	private final String PATH = "/challenge/dev-ps/submit-solution";
 	
 	private Client client;
 	
 	public RestClient() {
-		this.client= ClientBuilder.newClient();
+		this.client= ClientBuilder.newBuilder().register(MultiPartWriter.class).build();
 	}
 	
 	public int sendResult (String json, String token) {
 		
-		//TODO mudar submit pra formdata (no momento está incorreto)
+		MultiPart multipart = new MultiPart();
+		multipart.setMediaType(MediaType.MULTIPART_FORM_DATA_TYPE);
+		
+		InputStream stream = new ByteArrayInputStream(json.getBytes());
+		
+		StreamDataBodyPart part = new StreamDataBodyPart("answer", stream);
+		multipart.bodyPart(part);
 		
 		Response response = client
 				.target(this.URI)
 				.path(this.PATH)
 				.queryParam("token", token)
-				.request(json, MediaType.APPLICATION_JSON)
-				.post(Entity.entity(json, MediaType.APPLICATION_JSON));
+				.request( MediaType.APPLICATION_JSON)
+				.post(Entity.entity(multipart, MediaType.MULTIPART_FORM_DATA));
 		return response.getStatus();
 		
 		
